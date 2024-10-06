3d16bec72af70abb12b3fe2da43d8c5741cf7e01
Brycen Addison <b@brycenaddison.com>
2024-10-06 17:17:07 +1300

    feat(ui): add toast

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -3,3 +3,4 @@ import "./styles.css";
 export { Button } from "./components/button";
 export { Card } from "./components/card";
 export { Input } from "./components/input";
+export { Toaster, toast } from "./components/sonner";
