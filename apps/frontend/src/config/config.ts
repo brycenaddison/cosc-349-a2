4afd101a1d546fa06cbc59be9d14425af151e3ac
Brycen Addison <b@brycenaddison.com>
2024-10-07 15:23:23 +1300

    fix: fatal backend bug on new account

--- apps/frontend/src/config/config.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Config for Amazon Cognito user pool. This information is safe to be public. */
-const config = {
-  region: "us-east-1",
-  userPoolId: "us-east-1_KvEgTmwzv",
-  clientId: "45ndt7qmksmm1kt7ph0trkcpfn",
-};
-
-export default config;
